a0b959091de74abfc0249bab5088b590b58dbc14
Phu Huynh <phu@filesoft.tech>
2020-03-30 11:24:54 +0700

    Initial Commit

---
+++ TrackableScrollView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TrackableScrollView
+//
+//  Created by Huynh Tan Phu on 3/30/20.
+//  Copyright © 2020 Filesoft. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
